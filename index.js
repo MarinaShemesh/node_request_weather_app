79337d3edd627cd8cdd3a043100d3a8591a8eacf
MarinaShemesh <marina.shemesh1@gmail.com>
2018-07-02 22:23:23 +0300

    2july_1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-let apiKey = "d703871f861842b79c60988ccf3b17ec";
+let apiKey = "this is a secret";
 let city = 'haifa';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
