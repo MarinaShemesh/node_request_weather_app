4bcb098e81ceeb3a76057160a27db48b2b03b435
MarinaShemesh <marina.shemesh1@gmail.com>
2018-07-02 22:37:03 +0300

    2july_2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const request = require('request');
+const argv = require('yargs').argv;  
+// argv allows us to define variables from the command line.
 
-let apiKey = "this is a secret";
-let city = 'haifa';
+
+let apiKey = "This is a secret";
+// let city = 'Haifa';
+let city = argv.c || 'Haifa';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 
@@ -9,11 +13,12 @@ request(url, function (err, response, body) {
   
   if(err){
     console.log('error:', error);
-  } else {
-    // console.log('weather:', weather);
+
+  } else {    
    let weather = JSON.parse(body);
-   let message = `It's ${weather.main.temp} degrees in
-               ${weather.name}`;
+   // console.log('weather:', weather);
+   let message = `It's ${weather.main.temp} degrees Celsius in ${weather.name}
+                  and ${weather.clouds.all} percent cloudy`;
                
    console.log(message);
   }
