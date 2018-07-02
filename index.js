1833bade1305533e3500046add39d24d1ed55ba9
MarinaShemesh <marina.shemesh1@gmail.com>
2018-07-02 22:22:30 +0300

    2july_1

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const request = require('request');
 
 let apiKey = "d703871f861842b79c60988ccf3b17ec";
 let city = 'haifa';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 
-request(url, function (err, response, body) {
-  let weather = JSON.parse(body);
+request(url, function (err, response, body) { 
   
   if(err){
     console.log('error:', error);
   } else {
-    console.log('weather:', weather);
+    // console.log('weather:', weather);
+   let weather = JSON.parse(body);
+   let message = `It's ${weather.main.temp} degrees in
+               ${weather.name}`;
+               
+   console.log(message);
   }
+
 });
+
